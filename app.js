d4b3944f23339f2b57e3adf0e6569c9a71cfe089
xKyru <camc-107@hotmail.com>
2024-12-31 11:55:15 -0600

    updated

---
+++ app.js
@@ -0,0 +1,21 @@
+window.addEventListener("load", () => {
+    const countdownContainer = document.querySelector("#countdown");
+    const targetDate = new Date(1735700400000);
+    let hours;
+    let minutes;
+    let seconds;
+
+    function updateCountdown() {
+        let ts = countdown(targetDate);
+        hours = ts.hours.toString().padStart(2, "0");
+        minutes = ts.minutes.toString().padStart(2, "0");;
+        seconds = ts.seconds.toString().padStart(2, "0");;
+        countdownContainer.innerHTML = `${hours}:${minutes}:${seconds}`;
+    }
+
+    // Actualiza el countdown cada segundo
+    setInterval(updateCountdown, 1000);
+
+    // Llama a la función una vez para inicializar
+    updateCountdown();
+});
