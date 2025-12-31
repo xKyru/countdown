7c5e02cf6ffdb341433240bbaf42c29a32a5afdd
xKyru <57547654+xKyru@users.noreply.github.com>
2025-12-30 20:30:26 -0600

    Update countdown target date and refactor code

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 window.addEventListener("load", () => {
     const countdownContainer = document.querySelector("#countdown");
-    const targetDate = new Date(1767236400000);
-    let hours;
-    let minutes;
-    let seconds;
+    const targetDate = new Date("2026-01-01T00:00:00-03:00");
+
+    let timerId;
 
     function updateCountdown() {
-        let ts = countdown(targetDate);
-        hours = ts.hours.toString().padStart(2, "0");
-        minutes = ts.minutes.toString().padStart(2, "0");;
-        seconds = ts.seconds.toString().padStart(2, "0");;
+        let ts = countdown(new Date(), targetDate);     
+
+        if (ts.value <= 0) {
+            clearInterval(timerId);
+            countdownContainer.innerHTML = `<span>00</span>:<span>00</span>:<span>00</span>`;
+            return;
+        }
+
+        const hours = ts.hours.toString().padStart(2, "0");
+        const minutes = ts.minutes.toString().padStart(2, "0");
+        const seconds = ts.seconds.toString().padStart(2, "0");
+
         countdownContainer.innerHTML = `<span>${hours}</span>:<span>${minutes}</span>:<span>${seconds}</span>`;
     }
 
     // Actualiza el countdown cada segundo
-    setInterval(updateCountdown, 1000);
+    timerId = setInterval(updateCountdown, 1000);
 
     // Llama a la función una vez para inicializar
     updateCountdown();
